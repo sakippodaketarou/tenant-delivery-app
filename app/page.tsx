612d83fabe8da39ddf2650a7fa2b09f486ce9de0
yukiya <yukiya.k_0318@outlook.jp>
2026-04-30 00:36:23 +0900

    feat: auth redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const supabase = await createClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/home");
+  }
+
   redirect("/login");
 }
